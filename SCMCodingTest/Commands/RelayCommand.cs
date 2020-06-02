d906ee8ebb5238ffbcefa939f7d5c25f4417821d
bheemasekharj <66297418+bheemasekharj@users.noreply.github.com>
2020-06-02 12:26:58 +0530

    Add files via upload

---
+++ SCMCodingTest/Commands/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace LicenseGeneration
+{
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<T> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }       
+    }
+}
---
+++ SCMCodingTest/Utility/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCMCodingTest
+{
+    class Constants
+    {
+        public static double PromotionA=50;
+        public static double PromotionB=30;
+        public static double PromotionC=20;
+        public static double PromotionD=15;
+
+    }
+}
